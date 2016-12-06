8f05f78bbc33f4d00c21d9c51f0429dd459f7749
Shelby Doolittle <shelby@shelbyd.com>
2016-12-05 22:29:32 -0500

    separate tests

--- src/parser.rs
+++ src/parser.rs
@@ -19,7 +19,7 @@ named!(line<&[u8], Line>, ws!(key_value));
 
 named!(parse<&[u8], Vec<Line> >, many0!(line));
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub enum Line {
     KeyValue(String, String),
 }
@@ -53,4 +53,124 @@ pub fn get_lines(s: &str) -> Result<Vec<Line>, ParseError> {
         _ => {}
     }
     parsed.to_full_result().map_err(::std::convert::From::from)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn assert_syntax_error(contents: &str) {
+        assert_eq!(get_lines(contents).unwrap_err(),
+               ParseError::Syntax);
+    }
+
+    fn get_lines_vec(contents: &str) -> Vec<Line> {
+        get_lines(contents).unwrap()
+    }
+
+    fn key_value(key: &str, value: &str) -> super::Line {
+        super::Line::KeyValue(key.to_string(), value.to_string())
+    }
+
+    #[test]
+    fn root_level_empty() {
+        assert_eq!(get_lines_vec(""), vec![]);
+    }
+
+    #[test]
+    fn root_level_single_property() {
+        let result = vec![
+            key_value("hostname", "dynamo"),
+        ];
+        assert_eq!(get_lines("hostname = \"dynamo\"").unwrap(), result);
+    }
+
+    #[test]
+    fn root_level_three_properties() {
+        let result = vec![
+            key_value("hostname", "dynamo"),
+            key_value("port", "5153"),
+            key_value("path", "/foo/bar"),
+        ];
+        assert_eq!(
+            get_lines("hostname = \"dynamo\"\nport = \"5153\"\npath = \"/foo/bar\"").unwrap(),
+            result);
+    }
+
+    #[test]
+    fn just_a_word() {
+        assert_syntax_error("hostname");
+    }
+
+    #[test]
+    fn word_and_equals() {
+        assert_syntax_error("hostname = ");
+    }
+
+    #[test]
+    #[ignore]
+    fn root_level_property_with_many_dots_together() {
+        assert_syntax_error("host..name = \"dynamo\"");
+    }
+
+    #[test]
+    fn quoted_word_with_stuff_after() {
+        assert_syntax_error("hostname = \"dynamo\"great");
+        assert_syntax_error("hostname = \"dynamo\" great");
+    }
+
+    #[test]
+    fn root_level_property_with_dots() {
+        let result = vec![
+            key_value("host.name", "dynamo"),
+        ];
+        assert_eq!(get_lines("host.name = \"dynamo\"").unwrap(), result);
+    }
+
+    #[test]
+    fn root_level_property_with_underscores() {
+        let result = vec![
+            key_value("host_name", "dynamo"),
+        ];
+        assert_eq!(get_lines("host_name = \"dynamo\"").unwrap(), result);
+    }
+
+    #[test]
+    fn bare_words() {
+        let result = vec![
+            key_value("hostname", "dynamo"),
+        ];
+        assert_eq!(get_lines("hostname = dynamo").unwrap(), result);
+    }
+
+    #[test]
+    fn multiline_bare_words() {
+        let result = vec![
+            key_value("hostname", "dynamo"),
+            key_value("port", "5153"),
+            key_value("path", "/foo/bar"),
+        ];
+        assert_eq!(
+            get_lines("hostname = dynamo\nport = 5153\npath = /foo/bar").unwrap(),
+            result);
+    }
+
+    #[test]
+    fn bare_words_until_end_of_line() {
+        let result = vec![
+            key_value("hostname", "dynamo is bae"),
+        ];
+        assert_eq!(get_lines("hostname = dynamo is bae").unwrap(), result);
+    }
+
+    #[test]
+    #[ignore]
+    fn simple_multiline() {
+        // let result = vec![
+        //     Namespace("host".to_string(), vec![
+        //         key_value("name", "dynamo"),
+        //     ]),
+        // ];
+        // assert_eq!(get_lines("host\n    name = dynamo").unwrap(), result);
+    }
 }
--- tests/root.rs
+++ tests/root.rs
@@ -2,23 +2,6 @@ extern crate pureconfig;
 
 use pureconfig::Config;
 
-fn assert_syntax_error(contents: &str) {
-    assert_eq!(contents.parse::<Config>().unwrap_err(),
-               pureconfig::ParseError::Syntax);
-}
-
-#[test]
-fn root_level_empty() {
-    let config: Config = "".parse().unwrap();
-    assert_eq!(config.get("hostname"), None);
-}
-
-#[test]
-fn root_level_single_property() {
-    let config: Config = "hostname = \"dynamo\"".parse().unwrap();
-    assert_eq!(config.get("hostname"), Some("dynamo"));
-}
-
 #[test]
 fn root_level_three_properties() {
     let config: Config = "hostname = \"dynamo\"\nport = \"5153\"\npath = \"/foo/bar\""
@@ -28,54 +11,8 @@ fn root_level_three_properties() {
 }
 
 #[test]
-fn just_a_word() {
-    assert_syntax_error("hostname");
-}
-
-#[test]
-fn word_and_equals() {
-    assert_syntax_error("hostname = ");
-}
-
-#[test]
-fn root_level_property_with_dots() {
-    let config: Config = "host.name = \"dynamo\"".parse().unwrap();
-    assert_eq!(config.get("host.name"), Some("dynamo"));
-}
-
-#[test]
-fn root_level_property_with_underscores() {
-    let config: Config = "host_name = \"dynamo\"".parse().unwrap();
-    assert_eq!(config.get("host_name"), Some("dynamo"));
-}
-
-#[test]
 #[ignore]
-fn root_level_property_with_many_dots_together() {
-    assert_syntax_error("host..name = \"dynamo\"");
-}
-
-#[test]
-fn bare_words() {
-    let config: Config = "hostname = dynamo".parse().unwrap();
-    assert_eq!(config.get("hostname"), Some("dynamo"));
-}
-
-#[test]
-fn multiline_bare_words() {
-    let config: Config = "hostname = dynamo\nport = 5153\npath = /foo/bar".parse().unwrap();
-    assert_eq!(config.get("hostname"), Some("dynamo"));
-    assert_eq!(config.get("path"), Some("/foo/bar"));
-}
-
-#[test]
-fn bare_words_until_end_of_line() {
-    let config: Config = "hostname = dynamo is bae".parse().unwrap();
-    assert_eq!(config.get("hostname"), Some("dynamo is bae"));
-}
-
-#[test]
-fn quoted_word_with_stuff_after() {
-    assert_syntax_error("hostname = \"dynamo\"great");
-    assert_syntax_error("hostname = \"dynamo\" great");
+fn simple_multiline() {
+    let config: Config = "host\n    name = dynamo".parse().unwrap();
+    assert_eq!(config.get("host.name"), Some("dynamo"));
 }
