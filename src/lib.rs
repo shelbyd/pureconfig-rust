c5bf2478ca8223f726ed8939e4eff46ee8fde2c7
Shelby Doolittle <shelby@shelbyd.com>
2016-12-05 22:55:09 -0500

    add tests to src/lib

--- src/lib.rs
+++ src/lib.rs
@@ -18,6 +18,19 @@ impl Config {
     pub fn get(&self, property_name: &str) -> Option<&str> {
         self.map.get(property_name).map(|s| s.as_str())
     }
+
+    fn from_lines(lines: Vec<Line>) -> Config {
+        let mut map = HashMap::new();
+        for line in lines {
+            match line {
+                Line::KeyValue(key, value) => {
+                    map.insert(key, value);
+                }
+                Line::Comment(_) => {}
+            };
+        }
+        Config { map: map }
+    }
 }
 
 impl FromStr for Config {
@@ -25,14 +38,43 @@ impl FromStr for Config {
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let lines = try!(get_lines(s));
+        Ok(Config::from_lines(lines))
+    }
+}
 
-        let mut map = HashMap::new();
-        for line in lines {
-            match line {
-                Line::KeyValue(key, value) => map.insert(key, value),
-                Line::Comment(_) => panic!(),
-            };
-        }
-        Ok(Config { map: map })
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use super::parser::Line;
+
+    #[test]
+    fn single_key_value() {
+        let config = Config::from_lines(vec![Line::KeyValue("hostname".to_string(),
+                                                            "dynamo".to_string())]);
+        assert_eq!(config.get("hostname"), Some("dynamo"));
+    }
+
+    #[test]
+    fn missing_key_value() {
+        let config = Config::from_lines(vec![Line::KeyValue("hostname".to_string(),
+                                                            "dynamo".to_string())]);
+        assert_eq!(config.get("port"), None);
+    }
+
+    #[test]
+    fn many_key_values() {
+        let config =
+            Config::from_lines(vec![Line::KeyValue("hostname".to_string(), "dynamo".to_string()),
+                                    Line::KeyValue("port".to_string(), "5153".to_string()),
+                                    Line::KeyValue("path".to_string(), "/foo/bar".to_string())]);
+        assert_eq!(config.get("hostname"), Some("dynamo"));
+        assert_eq!(config.get("port"), Some("5153"));
+        assert_eq!(config.get("path"), Some("/foo/bar"));
+    }
+
+    #[test]
+    fn comments() {
+        let config = Config::from_lines(vec![Line::Comment("anything".to_string())]);
+        assert_eq!(config.get("hostname"), None);
     }
 }
